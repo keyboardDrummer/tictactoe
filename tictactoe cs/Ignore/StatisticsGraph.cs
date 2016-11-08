cca8d31dc69406acaf7d2fa3b4beb5f3b56324ab
Sjoerde <Sjoerd.vanEgmond@ortec-finance.com>
2016-11-08 16:17:31 +0100

    Axes and more games

--- tictactoe cs/Ignore/StatisticsGraph.cs
+++ tictactoe cs/Ignore/StatisticsGraph.cs
@@ -23,7 +23,7 @@ namespace tictactoe_cs.Ignore
 		}
 
 		readonly IList<Statistics> _statistics = new List<Statistics>();
-		const int GamesToPlay = 1000;
+		const int GamesToPlay = 10000;
 		const int MaxY = 1000;
 
 		protected override void OnPaint(PaintEventArgs e)
@@ -41,6 +41,15 @@ namespace tictactoe_cs.Ignore
 		{
 			int x = 0;
 			int y = 0;
+			graphics.DrawLine(new Pen(Brushes.Gray), new Point(0, 0), new Point(GamesToPlay, 0));
+			graphics.DrawLine(new Pen(Brushes.Gray), new Point(0, MaxY/2), new Point(GamesToPlay, MaxY/2));
+			graphics.DrawLine(new Pen(Brushes.Gray), new Point(0, MaxY), new Point(GamesToPlay, MaxY));
+			graphics.DrawLine(new Pen(Brushes.Gray), new Point(0, 0), new Point(0, MaxY));
+			foreach (int interval in Enumerable.Range(1, 9).Except(new[] {5}))
+			{
+				graphics.DrawLine(new Pen(Brushes.LightGray), new Point(0, MaxY/10*interval),
+					new Point(GamesToPlay, MaxY/10*interval));
+			}
 			foreach (var statistic in _statistics)
 			{
 				var newY = (int) (statistic.FirstWinPercentage*MaxY);
