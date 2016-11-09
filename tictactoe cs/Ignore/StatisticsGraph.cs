e08aec0577d9c992ca5d9247888b6c56c97a0548
Remy Willems <remy.willems@ortec-finance.com>
2016-11-09 13:35:21 +0100

    Cosmetics.

--- tictactoe cs/Ignore/StatisticsGraph.cs
+++ tictactoe cs/Ignore/StatisticsGraph.cs
@@ -32,7 +32,7 @@ namespace tictactoe_cs.Ignore
 			graphics.SmoothingMode = SmoothingMode.AntiAlias;
 			var margin = 10f;
 			graphics.TranslateTransform(margin, margin);
-			graphics.ScaleTransform((ClientSize.Width - 2*margin)/GamesToPlay, (ClientSize.Height - 2*margin)/MaxY);
+			graphics.ScaleTransform((ClientSize.Width - 2 * margin) / GamesToPlay, (ClientSize.Height - 2 * margin) / MaxY);
 			DrawOnTransformedGraphics(graphics);
 			base.OnPaint(e);
 		}
@@ -54,21 +54,21 @@ namespace tictactoe_cs.Ignore
 
 		static void DrawGrid(Graphics graphics)
 		{
-			graphics.DrawLine(new Pen(Brushes.Red), new Point(0, MaxY/2), new Point(GamesToPlay, MaxY/2));
+			graphics.DrawLine(new Pen(Brushes.Red), new Point(0, MaxY / 2), new Point(GamesToPlay, MaxY / 2));
 			graphics.DrawLine(new Pen(Brushes.Gray), new Point(0, 0), new Point(GamesToPlay, 0));
-			graphics.DrawLine(new Pen(Brushes.Gray), new Point(0, MaxY/2), new Point(GamesToPlay, MaxY/2));
+			graphics.DrawLine(new Pen(Brushes.Gray), new Point(0, MaxY / 2), new Point(GamesToPlay, MaxY / 2));
 			graphics.DrawLine(new Pen(Brushes.Gray), new Point(0, MaxY), new Point(GamesToPlay, MaxY));
 			graphics.DrawLine(new Pen(Brushes.Gray), new Point(0, 0), new Point(0, MaxY));
 			foreach (int interval in Enumerable.Range(1, 9).Except(new[] {5}))
 			{
-				graphics.DrawLine(new Pen(Brushes.LightGray), new Point(0, MaxY/10*interval),
-					new Point(GamesToPlay, MaxY/10*interval));
+				graphics.DrawLine(new Pen(Brushes.LightGray), new Point(0, MaxY / 10 * interval),
+					new Point(GamesToPlay, MaxY / 10 * interval));
 			}
 		}
 
 		static int DrawLinePart(Graphics graphics, double relativeNewY, int x, int y, int newX, Brush black)
 		{
-			var newY = (int) (relativeNewY*MaxY);
+			var newY = (int) (relativeNewY * MaxY);
 			graphics.DrawLine(new Pen(black), new Point(x, MaxY - y), new Point(newX, MaxY - newY));
 			return newY;
 		}
